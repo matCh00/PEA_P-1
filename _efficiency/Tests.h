1bc6eb59a422a57e6ff8bfc2553cf1ed0a9a3b1e
matCh00 <mateusz.chalik.g@gmail.com>
2021-10-31 18:33:33 +0100

    nowe pliki

---
+++ _efficiency/Tests.h
@@ -0,0 +1,17 @@
+// testowanie efektywności algorytmów
+
+#ifndef PEA_P_1_TESTS_H
+#define PEA_P_1_TESTS_H
+
+#include "BranchAndBound.h"
+#include "BruteForce.h"
+#include "DynamicProgramming.h"
+#include "../_structures/Graph.h"
+
+
+class Tests {
+
+};
+
+
+#endif
--- _main/Menu.h
+++ _main/Menu.h
@@ -3,10 +3,7 @@
 #ifndef PEA_P_1_MENU_H
 #define PEA_P_1_MENU_H
 
-#include "BranchAndBound.h"
-#include "BruteForce.h"
-#include "DynamicProgramming.h"
-#include "../_structures/Graph.h"
+#include "../_efficiency/Tests.h"
 
 using namespace std;
 
